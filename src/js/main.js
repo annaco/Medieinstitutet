6758023d06bada214763811ffb396fe11cdc8d76
annaco <conradsonanna@hotmail.com>
2018-02-20 11:48:39 +0100

    jquery mm

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,57 @@
 $(document).ready(function() {
 
-   if(deviceDetector.device == 'desktop')
-    console.log('desctop');
-  else{
-    if(deviceDetector.device == 'tablet')
-       console.log('tablet');
-    else
-       console.log('mobile');
+  var images =[];
+  var i = 0;
+ 
+  var jumbotronPicture =["jumbo1.jpg", "jumbo2.jpg", "jumbo3.jpg"];
+
+   showPictureAndText(i);
+  
+  
+    $('.first').click(function() {
+     
+      i = 0;
+      showPictureAndText(i);
+        
+    });
+
+    $('.second').click(function() {
+     
+       i = 1;
+        showPictureAndText(i);
+      
+    });
+    
+    $('.third').click(function() {
+     
+       i = 2;
+        showPictureAndText(i);
+     
+    });
+
+    function showPictureAndText(i) {
+    
+      console.log(i);
+      images = ("<img src=images/" + jumbotronPicture[i] + ">"+ "<br>"); 
+     
+     
+      document.getElementById('slideshowImages').innerHTML = images;
+     }
+
+   var pageWidth = $(window).width();
+   if (pageWidth > 1024) {
+     $('.apply').hide();
+   
+     $('.applyHover').mouseover(function() {
+       $('.apply').show();
+     });
+     $('.applyHover').mouseout(function() {
+       $('.apply').hide();
+     });
    }
+   else {
+     $('.apply').hide();
+   }
+     
  
-
- 
-/* $('.apply').hide();
-  $('.applyHover').mouseenter (function() {
-   $('.apply').show();
-  }); 
-   $('.applyHover').mouseleave (function() {
-   $('.apply').hide();
-  });  */
-
 });
