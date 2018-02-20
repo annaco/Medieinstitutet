a5dbc5974623ea28c28dd84024ee5bcaef588dec
annaco <conradsonanna@hotmail.com>
2018-02-20 17:33:23 +0100

    utbildningar mm

--- src/js/main.js
+++ src/js/main.js
@@ -4,42 +4,50 @@ $(document).ready(function() {
   var i = 0;
  
   var jumbotronPicture =["jumbo1.jpg", "jumbo2.jpg", "jumbo3.jpg"];
+  var jumbotronText = [
+      ["Ansökan öppen", "Nu kan du ansöka till höstens utbildningar"],
+      ["YH-UTBILDNINGAR", "Läs mer om våra yh-utbildningar"],
+      [ "ÖPPET HUS ", "Välkommen till Öppet hus på Medieinstitutet i mars"]
+  ]
 
-   showPictureAndText(i);
+  showPictureAndText(i);
   
   
-    $('.first').click(function() {
-     
-      i = 0;
-      showPictureAndText(i);
-        
-    });
+  $('.first').click(function() {
+    i = 0;
+    showPictureAndText(i);
+  });
 
-    $('.second').click(function() {
-     
-       i = 1;
-        showPictureAndText(i);
-      
-    });
+  $('.second').click(function() {
+    i = 1;
+    showPictureAndText(i);
+  });
     
-    $('.third').click(function() {
-     
-       i = 2;
-        showPictureAndText(i);
-     
-    });
+  $('.third').click(function() {
+    i = 2;
+    showPictureAndText(i);
+  });
 
-    function showPictureAndText(i) {
+  $('.second').click(function() {
+    i = 1;
+    showPictureAndText(i);
+  });
+
+  function showPictureAndText(i) {
     
-      console.log(i);
-      images = ("<img src=images/" + jumbotronPicture[i] + ">"+ "<br>"); 
-     
-     
-      document.getElementById('slideshowImages').innerHTML = images;
-     }
+     console.log(i);
+     images = ("<img src=images/" + jumbotronPicture[i] + ">"+ "<br>"); 
+     jumbotronText = (jumbotronText[i][1] + "<br>");
+          
+     document.getElementById('slideshowImages').innerHTML = images;
+     document.getElementById('slideshowText').innerHTML = jumbotronText;
+  }
 
+
+
+   //Gör så att innehållet under ansökan endast visas föär desktop
    var pageWidth = $(window).width();
-   if (pageWidth > 1024) {
+   if (pageWidth > 1019) {
      $('.apply').hide();
    
      $('.applyHover').mouseover(function() {
